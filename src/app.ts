e404b42c6912cba98743988d9f5cd251fc2347f0
atetheone <atevirran@gmail.com>
2023-01-05 19:07:50 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { indexRouter } from "./routes";
+import { apiRouter } from "./routes/api";
 
 require("dotenv").config();
 
 const app = express();
 
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/public", express.static(process.cwd() + "/src/public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
+app.use(indexRouter);
+app.use(apiRouter)
 
 export default app;
